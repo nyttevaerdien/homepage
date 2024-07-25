2d601745734ad76cc5721d1d873173af0cbf71c1
Kirill Fedotov <kirill.fedotov@klarna.com>
2024-07-25 21:48:25 +0200

    added h2 database instead of postgre

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 ./gradlew --build-file model/build.gradle build
 ./gradlew --build-file api/build.gradle jibDockerBuild
 ./gradlew --build-file frontend/build.gradle jibDockerBuild
-docker push nyttevaerdien/homepage-frontend:1.1.0
-docker push nyttevaerdien/homepage-api:1.1.0
+docker push nyttevaerdien/homepage-frontend:1.2.0
+docker push nyttevaerdien/homepage-api:1.2.0
